c303858f824d5e33d90f2bc638ededca0a58ed16
bagusistighfar86 <79921639+bagusistighfar86@users.noreply.github.com>
2021-05-21 22:57:46 +0700

    Update soal2a.c

--- soal2a.c
+++ soal2a.c
@@ -8,38 +8,38 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-int baris=4, bk=3, kolom=6;
-int matriks1[baris][bk], matriks2[bk][kolom], matriks_hasil[baris * kolom];
+#define R 4
+#define RC 3
+#define C 6
 
-void* kali(void* argvar) {
+void* mul(void* argvar) {
     int* arg = (int*)argvar;
     int i, n = 0;
 
-    for (i = 0; i < bk; i++) 
-        n += arg[i] * arg[i + bk];
+    for (i = 1; i <= RC; i++) n += arg[i] * arg[i + RC];
 
     int* p = (int*)malloc(sizeof(int));
     *p = n;
     pthread_exit(p);
 }
 
-int main() {
-    key_t key = 1234;
+int m1[R][RC], m2[RC][C], m_res[R * C];
 
-    int shmid = shmget(key, sizeof(int) * baris * kolom, IPC_CREAT | 0666);
-    int* share_hasil = (int*)shmat(shmid, NULL, 0);
+int main() {
+    key_t key = 1945;
+
+    int shmid = shmget(key, sizeof(int) * R * C, IPC_CREAT | 0666);
+    int* res = (int*)shmat(shmid, NULL, 0);
 
     int i, j;
-    printf("Matrix %d x %d :\n", baris, bk);
-    for (i = 0; i < baris; i++) {
-        for (j = 0; j < bk; j++) 
-            scanf("%d", &matriks1[i][j]);
+    printf("Matrix %dx%d :\n", R, RC);
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < RC; j++) scanf("%d", &m1[i][j]);
     }
 
-    printf("\nMatrix %d x %d :\n", bk, kolom);
-    for (i = 0; i < bk; i++) {
-        for (j = 0; j < kolom; j++) 
-            scanf("%d", &matriks2[i][j]);
+    printf("\nMatrix %dx%d :\n", RC, C);
+    for (i = 0; i < RC; i++) {
+        for (j = 0; j < C; j++) scanf("%d", &m2[i][j]);
     }
 
     int err, k;
@@ -48,42 +48,42 @@ int main() {
     int count = 0;
     int* arg = NULL;
 
-    for (i = 0; i < baris; i++) {
-        for (j = 0; j < kolom; j++) {
+    for (i = 0; i < R; i++) {
+        for (j = 0; j < C; j++) {
             arg = (int*)malloc((24) * sizeof(int));
-            arg[0] = bk;
+            arg[0] = RC;
 
-            for (k = 0; k < bk; k++) {
-                arg[k + 1] = matriks1[i][k];
+            for (k = 0; k < RC; k++) {
+                arg[k + 1] = m1[i][k];
             }
-            for (k = 0; k < bk; k++) {
-                arg[k + bk + 1] = matriks2[k][j];
+            for (k = 0; k < RC; k++) {
+                arg[k + RC + 1] = m2[k][j];
             }
 
-            err = pthread_create(&(tid[count++]), NULL, &kali, (void*)arg);
+            err = pthread_create(&(tid[count++]), NULL, &mul, (void*)arg);
             if (err != 0)
                 printf("\n can't create thread : [%s]", strerror(err));
         }
     }
 
     printf("\nHasil Perkalian :\n");
-    for (i = 0; i < baris * kolom; i++) {
+    for (i = 0; i < R * C; i++) {
         void* k;
         pthread_join(tid[i], &k);
         int* p = (int*)k;
 
         printf("%d ", *p);
-        if ((i + 1) % kolom == 0) {
+        if ((i + 1) % C == 0) {
             printf("\n");
         }
-        matriks_hasil[i] = *p;
+        m_res[i] = *p;
     }
 
-    for (i = 0; i < baris * kolom; i++) {
-        share_hasil[i] = matriks_hasil[i];
+    for (i = 0; i < R * C; i++) {
+        res[i] = m_res[i];
     }
 
-    shmdt(share_hasil);
+    shmdt(res);
     exit(0);
     return 0;
 }
