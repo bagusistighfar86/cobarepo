f156a80143583cffc6621de47bc15984ecb0351a
bagusistighfar86 <79921639+bagusistighfar86@users.noreply.github.com>
2021-05-22 00:31:33 +0700

    Update soal2c.c

--- soal2c.c
+++ soal2c.c
@@ -3,43 +3,39 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void closeAll(int fd1[2], int fd2[2]) {
-    close(fd1[0]);
-    close(fd1[1]);
-    close(fd2[0]);
-    close(fd2[1]);
+void closeAll(int fd[][2]) {
+    close(fd[0][0]);
+    close(fd[0][1]);
+    close(fd[1][0]);
+    close(fd[1][1]);
 }
 
 int main() {
-    int fd1[2], fd2[2];
+    int status;
+    int fd[2][2];
     char *ps_arg[] = {"ps", "aux", NULL};
     char *sort_arg[] = {"sort", "-nrk", "3,3", NULL};
     char *head_arg[] = {"head", "-5", NULL};
 
-    pipe(fd1);
-    pipe(fd2);
+    pipe(fd[0]);
+    pipe(fd[1]);
 
     if (fork() == 0) {
-        dup2(fd1[1], STDOUT_FILENO);
-        closeAll(fd1,fd2);
+        dup2(fd[0][1], STDOUT_FILENO);
+        closeAll(fd);
         execv("/bin/ps", ps_arg);
     } else if (fork() == 0) {
-        dup2(fd1[0], STDIN_FILENO);
-        dup2(fd2[1], STDOUT_FILENO);
-        closeAll(fd1,fd2);
+        dup2(fd[0][0], STDIN_FILENO);
+        dup2(fd[1][1], STDOUT_FILENO);
+        closeAll(fd);
         execv("/bin/sort", sort_arg);
     } else if (fork() == 0) {
-        dup2(fd2[0], STDIN_FILENO);
-        closeAll(fd1,fd2);
+        dup2(fd[1][0], STDIN_FILENO);
+        closeAll(fd);
         execv("/bin/head", head_arg);
     }
-    closeAll(fd1,fd2);
+    closeAll(fd);
 
-    int i, status;
-    for (i = 0; i < 3; i++) 
-        wait(&status);
+    for (int i = 0; i < 3; i++) wait(&status);
     return 0;
 }
-
-// STDOUT_FILENO = 1;
-// STDIN_FILENO = 0;
